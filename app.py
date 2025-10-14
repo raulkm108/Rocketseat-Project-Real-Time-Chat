eb3afa36feab03bcde96fb20d68b71bbb0d42732
raulkm108 <raulkm.civil@gmail.com>
2025-10-14 20:33:24 -0300

    socketio.on
    implemented the function to keep the chat on while we type, added the lib emit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 
@@ -8,6 +8,10 @@ socketio = SocketIO(app)
 @app.route('/chat', methods=['GET'])
 def generate_chat():
     return render_template('index.html')
+
+@socketio.on('message')
+def handle_message(msg):
+    emit('message', msg, broadcast=True)
 
 
 if __name__ == '__main__':
