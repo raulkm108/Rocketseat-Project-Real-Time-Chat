db3f47a707e202c39bb924fb9a57573c897d71e5
raulkm108 <raulkm.civil@gmail.com>
2025-10-14 20:25:36 -0300

    Rendering template and changed from post to get

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ app = Flask(__name__)
 
 socketio = SocketIO(app)
 
-@app.route('/chat', methods=['POST'])
+@app.route('/chat', methods=['GET'])
 def generate_chat():
-    
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     socketio.run(app,debug=True)
